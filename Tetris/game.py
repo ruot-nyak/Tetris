a0b1746906e274fbd589dd4b88e586a86b01a699
ruot-nyak <nyak.g.ruot@gmail.com>
2020-10-24 11:48:25 -0700

    defined the pieces, implemented random shapes to fall and creating the grid

---
+++ Tetris/game.py
@@ -0,0 +1,195 @@
+import pygame
+import random
+
+# creating the data structure for pieces
+# setting up global vars
+# functions
+# - create_grid
+# - draw_grid
+# - draw_window
+# - rotating shape in main
+# - setting up the main
+
+"""
+10 x 20 square grid
+shapes: S, Z, I, O, J, L, T
+represented in order by 0 - 6
+"""
+
+pygame.font.init()
+
+# GLOBALS VARS
+s_width = 800
+s_height = 700
+play_width = 300  # meaning 300 // 10 = 30 width per block
+play_height = 600  # meaning 600 // 20 = 20 height per block
+block_size = 30
+
+top_left_x = (s_width - play_width) // 2
+top_left_y = s_height - play_height
+
+
+# SHAPE FORMATS
+# Marking the possible positions of the shapes, it's a grid system where the 0's indicate where the block is.
+S = [['.....',
+      '......',
+      '..00..',
+      '.00...',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..00.',
+      '...0.',
+      '.....']]
+
+Z = [['.....',
+      '.....',
+      '.00..',
+      '..00.',
+      '.....'],
+     ['.....',
+      '..0..',
+      '.00..',
+      '.0...',
+      '.....']]
+
+I = [['..0..',
+      '..0..',
+      '..0..',
+      '..0..',
+      '.....'],
+     ['.....',
+      '0000.',
+      '.....',
+      '.....',
+      '.....']]
+
+O = [['.....',
+      '.....',
+      '.00..',
+      '.00..',
+      '.....']]
+
+J = [['.....',
+      '.0...',
+      '.000.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..00.',
+      '..0..',
+      '..0..',
+      '.....'],
+     ['.....',
+      '.....',
+      '.000.',
+      '...0.',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..0..',
+      '.00..',
+      '.....']]
+
+L = [['.....',
+      '...0.',
+      '.000.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..0..',
+      '..00.',
+      '.....'],
+     ['.....',
+      '.....',
+      '.000.',
+      '.0...',
+      '.....'],
+     ['.....',
+      '.00..',
+      '..0..',
+      '..0..',
+      '.....']]
+
+T = [['.....',
+      '..0..',
+      '.000.',
+      '.....',
+      '.....'],
+     ['.....',
+      '..0..',
+      '..00.',
+      '..0..',
+      '.....'],
+     ['.....',
+      '.....',
+      '.000.',
+      '..0..',
+      '.....'],
+     ['.....',
+      '..0..',
+      '.00..',
+      '..0..',
+      '.....']]
+
+shapes = [S, Z, I, O, J, L, T]
+shape_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 165, 0), (0, 0, 255), (128, 0, 128)]
+# index 0 - 6 represent shape
+
+
+class Piece(object):
+    def __init__(self,x,y,shape):
+        self.x = x
+        self.y = y
+        self.shape = shape 
+        self.color = shape_colors[shapes.index(shape)]
+        self.rotation = 0
+
+
+def create_grid(locked_pos={}):
+    #have 10 colored square in each collum and 20 horizontal rows
+    grid = [[(0,0,0) for x in range(10)] for x in range (20)]
+
+    for i in range(len(grid[i])):
+        for j in range(len(grid[i])):
+        #rows are represented by i and collumns are represented with a j
+            if (j,i) in locked_pos:
+                c = locked_pos[(j,i)]
+            grid [i] [j] = c
+    return grid
+
+def convert_shape_format(shape):
+    pass
+
+def valid_space(shape, grid):
+    pass
+
+def check_lost(positions):
+    pass
+
+def get_shape():
+    return random.choice(shapes)
+
+def draw_text_middle(text, size, color, surface):  
+    pass
+   
+def draw_grid(surface, row, col):
+    pass
+
+def clear_rows(grid, locked):
+    pass
+
+def draw_next_shape(shape, surface):
+    pass
+
+def draw_window(surface):
+    pass
+
+def main():
+    pass
+
+def main_menu():
+    pass
+
+main_menu()  # start game
